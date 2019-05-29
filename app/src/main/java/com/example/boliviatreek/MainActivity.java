3161013448dafc249ed446cbac3c1b675bf9489c
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-05-28 22:24:46 -0400

    intento de firestore y listas

--- app/src/main/java/com/example/boliviatreek/MainActivity.java
+++ app/src/main/java/com/example/boliviatreek/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.boliviatreek;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
@@ -17,28 +26,69 @@ public class MainActivity extends AppCompatActivity {
     //private TextView tv_title, tv_ubicacion, tv_kilometraje, tv_distancia, tv_modalidad, tv_dificultad, tv_valoracion, prueba;
     private ListView lv1;
     private TabHost th;
-    private String[][] datos_txt = {
-            {"texto 1", "texto 1", "texto 1", "texto 1", "texto 1", "texto 1", "texto 1"},
-            {"texto 2", "texto 2", "texto 2", "texto 2", "texto 2", "texto 2", "texto 2"},
-            {"texto 3", "texto 3", "texto 3", "texto 3", "texto 3", "texto 3", "texto 3"},
-            {"texto 4", "texto 4", "texto 4", "texto 4", "texto 4", "texto 4", "texto 4"},
-            {"texto 5", "texto 5", "texto 5", "texto 5", "texto 5", "texto 5", "texto 5"},
-            {"texto 6", "texto 6", "texto 6", "texto 6", "texto 6", "texto 6", "texto 6"},
-            {"texto 7", "texto 7", "texto 7", "texto 7", "texto 7", "texto 7", "texto 7"},
-            };
-    private int[] datos_img = {R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,};
+
+    // para la base de datos
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+
+    private String[][] datos_txt2 = {{"uno","uno","uno","uno","uno","uno","uno"},
+            {"uno","uno","uno","uno","uno","uno","uno"},
+            {"dos","uno","dos","uno","uno","uno","uno"},
+            {"uno","uno","uno","uno","uno","uno","uno"},
+            {"uno","uno","uno","uno","uno","uno","uno"},
+            {"uno","uno","uno","uno","uno","uno","uno"},
+            {"uno","uno","uno","uno","uno","uno","uno"}} ;
+    //private String[][] datos_txt;
+    private int[] datos_img2 = {R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,};
+
+
     private String ubicaciones [] = {"La Paz", "Oruro"};
+
+
+    public int contador = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //database
+        //int x = db.collection("Rutas")
+        db.collection("Rutas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
-        lv1 = (ListView) findViewById(R.id.lv_listaRutas);
-        lv1.setAdapter(new Adaptador(this,this.datos_txt,this.datos_img));
-        th =(TabHost) findViewById(R.id.th_principal);
+                        if (task.isSuccessful()) {
+
+                            for (QueryDocumentSnapshot document : task.getResult())
+                            {
+
+                                contador++;
+
+                                Log.d("***PRUEBA CONTADOR ***", document.getId() + " => " + document.getData());
+                            }
+                            funcionAux(contador);
+
+                        } else {
+                            //Log.w("", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        //contador++;
+        //Toast.makeText(MainActivity.this, "CONTADOR = "+contador, Toast.LENGTH_SHORT).show();
+
+        //lv1 = (ListView) findViewById(R.id.lv_listaRutas);
+        //lv1.setAdapter(new Adaptador(this,this.datos_txt2,this.datos_img));
+        //th =(TabHost) findViewById(R.id.th_principal);
 
         // ___________ inicia la configuracion de TABHOST____________//
+
+        th =(TabHost) findViewById(R.id.th_principal);
+
+        lv1 = (ListView) findViewById(R.id.lv_listaRutas);
         //***tab1
         th.setup(); //configuracion
         TabHost.TabSpec ts1 = th.newTabSpec("TabSpec1"); //se establece primero un tabspec relacionado con el tahost , el string es para identificarlo
@@ -63,5 +113,44 @@ public class MainActivity extends AppCompatActivity {
 
         th.addTab(ts3);
         //***
+    }
+
+
+    public void funcionAux (final int conti)
+    {
+
+        db.collection("Rutas")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    private String[][] datos_txt = new String[conti][7];
+                    private int[] datos_img = {R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,R.mipmap.icontrek,};
+
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+
+                        if (task.isSuccessful()) {
+                            int i=0;
+
+                            for (QueryDocumentSnapshot document : task.getResult())
+                            {
+                                datos_txt[i][0] = document.getData().get("txt1").toString();
+                                datos_txt[i][1] = document.getData().get("txt2").toString();
+                                datos_txt[i][2] = document.getData().get("txt3").toString();
+                                datos_txt[i][3] = document.getData().get("txt4").toString();
+                                datos_txt[i][4] = document.getData().get("txt5").toString();
+                                datos_txt[i][5] = document.getData().get("txt6").toString();
+                                datos_txt[i][6] = document.getData().get("txt7").toString();
+                                i++;
+
+                            }
+                            lv1.setAdapter(new Adaptador(getApplication(),datos_txt,this.datos_img));
+                        } else {
+                            //Log.w("", "Error getting documents.", task.getException()); document.getData()
+                        }
+                    }
+                });
+
+        //lv1.setAdapter(new Adaptador(getApplication(),datos_txt2,this.datos_img2));
     }
 }
