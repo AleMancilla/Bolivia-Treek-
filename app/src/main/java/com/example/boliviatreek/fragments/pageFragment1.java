d8d6cef9a9d3f26c0e937de881c733034d144e86
Alejandro Mancilla <alejandro.mancilla.umsa@gmail.com>
2019-06-10 12:44:23 -0400

    agregamos que los comentarios se guarden en la base de datos duncionando correctamente

--- app/src/main/java/com/example/boliviatreek/fragments/pageFragment1.java
+++ app/src/main/java/com/example/boliviatreek/fragments/pageFragment1.java
@@ -9,22 +9,32 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.format.Time;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.boliviatreek.AdapterDatosComentarios;
 import com.example.boliviatreek.Comentario;
-import com.example.boliviatreek.MainActivity22;
 import com.example.boliviatreek.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @SuppressLint("ValidFragment")
@@ -36,6 +46,12 @@ public class pageFragment1 extends Fragment {
     private TextView textView_texto_mitos;
     private TextView textView_como_llegar_info;
     private TextView textView_texto_como_llegar;
+
+    private EditText editText_deja_comentarios;
+    private Button button_comentar;
+    private FirebaseAuth mAuth;
+
+    private   String fecha_registro_sistema;
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();;
 
@@ -68,6 +84,7 @@ public class pageFragment1 extends Fragment {
         recyclerView = rootView.findViewById(R.id.ReciclerComentarios);
         recyclerView.setLayoutManager(new LinearLayoutManager(context));
 
+        mAuth = FirebaseAuth.getInstance();
 
         Time fecha_registro = new Time(Time.getCurrentTimezone());
         fecha_registro.setToNow();
@@ -75,7 +92,7 @@ public class pageFragment1 extends Fragment {
         int imes= fecha_registro.month+1;
         int iyear = fecha_registro.year;
 
-        String fecha_registro_sistema= idia + "/" + imes + "/" + iyear;
+        fecha_registro_sistema= idia + "/" + imes + "/" + iyear;
         llenarComentarios(fecha_registro_sistema);
 
         AdapterDatosComentarios adapter = new AdapterDatosComentarios(listComentarios);
@@ -100,6 +117,9 @@ public class pageFragment1 extends Fragment {
         textView_texto_mitos = rootView.findViewById(R.id.textView_texto_mitos);
         textView_como_llegar_info = rootView.findViewById(R.id.textView_como_llegar_info);
         textView_texto_como_llegar = rootView.findViewById(R.id.textView_texto_como_llegar);
+
+        editText_deja_comentarios = rootView.findViewById(R.id.editText_deja_comentario);
+        button_comentar = rootView.findViewById(R.id.button_comentar);
 
 
 
@@ -139,6 +159,12 @@ public class pageFragment1 extends Fragment {
 
 
 
+        button_comentar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                comentar(v);
+            }
+        });
 
         return rootView;
         //return super.onCreateView(inflater, container, savedInstanceState);
@@ -156,6 +182,63 @@ public class pageFragment1 extends Fragment {
         listComentarios.add(new Comentario( url_perfil,  nickname,  comentario,  fecha_registro_sistema));
 
 
+    }
+
+    public void comentar (View v)
+    {
+
+        final String comentario_caja = editText_deja_comentarios.getText().toString();
+
+        final String[] nickname = new String[1];
+        String user = FirebaseAuth.getInstance().getCurrentUser().getEmail();
+
+        //////////////////////////////////////////
+        db.collection("Usuarios").whereEqualTo("Email",user)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+                                nickname[0] = document.getData().get("nickname").toString();
+                                Toast.makeText(context, "Se agrego el comentario correctamente " , Toast.LENGTH_SHORT).show();
+                                comentarioenbd(nickname,comentario_caja);
+                                editText_deja_comentarios.setText("");
+                            }
+                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
+        ///////////////////////////////////////////
+
+
+    }
+    public void comentarioenbd(String[] nickname, String comentario_caja)
+    {
+        // Create a new user with a first, middle, and last name
+        Map<String, Object> comentario = new HashMap<>();
+        comentario.put("nickname", nickname[0]);
+        comentario.put("Fecha de comentario",    fecha_registro_sistema);
+        comentario.put("Comentario", comentario_caja);
+//        comentario.put("born", 1912);
+
+// Add a new document with a generated ID
+        db.collection("Comentarios")
+                .add(comentario)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+//                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 
 
